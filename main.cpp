66e8f7e3eecfdc7da5ca102244af03625c7368ea
Pritesh <pritesh.patankar@gmail.com>
2015-04-02 11:00:21 +0530

    Noraml Function added to caluckate area by Pritesh

--- main.cpp
+++ main.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+int width;
+int height;
+void setvalues(int a, int b);
+int area();
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Working in TempBranch2" << endl;
+
+    setvalues(1,2);
+    cout<<area();
     return 0;
 }
 
+void setvalues(int a, int b){
+    width = a ;
+    height= b;
+}
+int area(){
+    return width * height;
+}
