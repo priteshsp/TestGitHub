dc7f33e1378b1884c1efd1dcffd6ee049cf7ec1d
Pritesh <pritesh.patankar@gmail.com>
2015-04-02 10:54:05 +0530

    added test class and setvalue, area method to calculate area by Pritesh

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-
+#include "funcitontest.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "First Program" << endl;
+    test obj1;
+    obj1.setvalues(12,2);
+    obj1.area();
+    cout<<obj1.area();
+
     return 0;
 }
+
 
--- testGIT.pro
+++ testGIT.pro
@@ -8,3 +8,6 @@ SOURCES += main.cpp
 include(deployment.pri)
 qtcAddDeployment()
 
+HEADERS += \
+    funcitontest.h
+
